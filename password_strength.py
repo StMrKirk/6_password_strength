5f1d6ea4b40db7dc23adab9829cbaf50a3a95dd2
StMrKirk <disaseractiv@gmail.com>
2017-09-10 16:17:03 +0300

    Some changes for main function (made it less complicated)

--- password_strength.py
+++ password_strength.py
@@ -2,9 +2,8 @@ import re
 import sys
 
 
-'''Господи боже мой скок я разбирался с этим regex я надеюсь что-то я запомнил: соотвествие или строчной или заглавной 
-или цифре, цифре и строчной и заглавной, цифре и строчной и заглавной и спец.знаку, двум цифрам и двум заглавным и двум
-строчным и двум спец.знакам'''
+'''соотвествие или строчной или заглавной или цифре, цифре и строчной и заглавной, цифре и строчной 
+и заглавной и спец.знаку, двум цифрам и двум заглавным и двумстрочным и двум спец.знакам'''
 password_patterns_list = list(['^(\d|[A-Z]|[a-z]|[!@#$&*%,./:;^_=?\(\)\{\}\[\]])$',
                                '^(?=.*?\d)(?=.*?[A-Z])(?=.*?[a-z])[A-Za-z\d]+$',
                                '^(?=.*?\d)(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[!@#$&*%,./:;^_=?\(\)\{\}\[\]])'
@@ -14,14 +13,19 @@ password_patterns_list = list(['^(\d|[A-Z]|[a-z]|[!@#$&*%,./:;^_=?\(\)\{\}\[\]])
                                '.*[!@#$&*%,./:;^_=?\(\)\{\}\[\]])[A-Za-z\d!@#$&*%,./:;^_=?\(\)\{\}\[\]]+$'])
 
 
-def get_password_strength(password):
-    """Функция проверяет наличие пароля в файле с самыми популярными паролями и выводит его сложность согласно
-    условию(паттерну)"""
-    strength = 1
+def check_password_in_top_list(password):
+    """Функция проверяет наличие пароля в самых популярных"""
     with open('10_million_password_list_top_10000.txt', 'r', encoding='UTF-8') as password_list:
         for password_list_line in password_list:
             if password_list_line.rstrip() == password:
-                return strength
+                return True
+
+
+def get_password_strength(password):
+    """Функция выводит сложность пароля согласно условию(паттерну)"""
+    strength = 1
+    if check_password_in_top_list(password):
+        return strength
     for password_pattern in password_patterns_list:
         if re.search(password_pattern, password):
             strength = password_patterns_list.index(password_pattern)+1
