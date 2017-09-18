ee4204c509803196861e26d75a89272e492ed5c3
StMrKirk <disaseractiv@gmail.com>
2017-09-19 00:42:34 +0300

    Add regex discription, separate loading data from checking in data

--- password_strength.py
+++ password_strength.py
@@ -3,19 +3,31 @@ import sys
 
 
 password_patterns_list = list(['^(\d|[A-Z]|[a-z]|[!@#$&*%,./:;^_=?\(\)\{\}\[\]])$',
+                               # наличие либо заглавной либо строчной либо спецсимвола либо цифры
                                '^(?=.*?\d)(?=.*?[A-Z])(?=.*?[a-z])[A-Za-z\d]+$',
+                               # наличие цифры и заглавной и строчной
                                '^(?=.*?\d)(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[!@#$&*%,./:;^_=?\(\)\{\}\[\]])'
                                '[A-Za-z\d!@#$&*%,./:;^_=?\(\)\{\}\[\]]+$',
+                               # наличие цифры и заглавной и строчной и спецсимвола
                                '^(?=.*?\d.*\d)(?=.*?[A-Z].*[A-Z])(?=.*?[a-z].*[a-z])'
                                '(?=.*?[!@#$&*%,./:;^_=?\(\)\{\}\[\]]'
                                '.*[!@#$&*%,./:;^_=?\(\)\{\}\[\]])[A-Za-z\d!@#$&*%,./:;^_=?\(\)\{\}\[\]]+$'])
+                               # наличие 2 цифр и 2 заглавных и 2 строчных и 2 спецсимволов
+
+
+def load_password_top_list():
+    password_list = []
+    with open('10_million_password_list_top_10000.txt', 'r', encoding='UTF-8') as password_file:
+        for password_file_line in password_file:
+            password_list.append(password_file_line.rstrip())
+        return password_list
 
 
 def check_password_in_top_list(password):
-    with open('10_million_password_list_top_10000.txt', 'r', encoding='UTF-8') as password_list:
-        for password_list_line in password_list:
-            if password_list_line.rstrip() == password:
-                return True
+    password_list = load_password_top_list()
+    for password_list_item in password_list:
+        if password_list_item == password:
+            return True
 
 
 def get_password_strength(password):
@@ -27,7 +39,7 @@ def get_password_strength(password):
             strength = password_patterns_list.index(password_pattern)+1
     if len(password) >= 8 and strength != 1 and strength != 2:
         strength += 6
-    elif strength != 1 and strength != 2 and strength != 3:
+    elif strength not in (1, 2, 3):
         strength += len(password)
     return strength
 
